968094db6e0cfa4e72b1fc8f95f66da484aea50a
Charity <charitywanjiku8245@gmail.com>
2024-06-10 13:48:49 +0300

    CodeChallenge Complete

--- models/author.py
+++ models/author.py
@@ -1,7 +1,49 @@
+import sqlite3
+
 class Author:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
+    def __init__(self, name):
+        if not isinstance(name, str) or len(name) == 0:
+            raise ValueError("Name must be a non-empty string")
+        self._name = name
+        self._id = None  # The ID will be assigned by the database
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, id):
+        if isinstance(id, int):
+            self._id = id
+        else:
+            raise ValueError("Author id must be of type int")
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if hasattr(self, '_name'):
+            raise AttributeError("Cannot change the name after the author is instantiated.")
+        elif isinstance(name, str) and len(name) > 0:
+            self._name = name
+        else:
+            raise ValueError("Name must be of type str and non-empty.")
+
+    def create_author(self):
+        conn = sqlite3.connect('./database/magazine.db')
+        cursor = conn.cursor()
+
+        cursor.execute('INSERT INTO authors (name) VALUES (?)', (self._name,))
+        self._id = cursor.lastrowid
+        conn.commit()
+        conn.close()
+
+    def test_author_creation(self):
+        author = Author("John Doe")  # Only pass the name argument
+        assert author.name == "John Doe"
+
 
     def __repr__(self):
-        return f'<Author {self.name}>'
+        return f'<Author {self._name}>'
